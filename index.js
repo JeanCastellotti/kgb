898d748a8cc0caea458cf80e9d0abebc13ba2a95
JeanDevFR <42603119+JeanDevFR@users.noreply.github.com>
2021-09-30 21:02:11 +0200

    Initial template

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
+const stylus = require('stylus');
 
 const app = express();
 
-app.listen(() => {
+// mongoose
+// mongoose
+// 	.connect('mongodb://localhost:27017/kgb')
+// 	.then(() => {
+// 		console.log('Database successfully connected');
+// 	})
+// 	.catch((err) => {
+// 		console.error(err);
+// 	});
+
+// view engine
+app.engine('hbs', exphbs({ extname: 'hbs' }));
+app.set('view engine', 'hbs');
+
+// body parser
+app.use(express.urlencoded({ extended: true }));
+
+// stylus
+app.use(stylus.middleware(path.join(__dirname, 'public')));
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+	res.render('index', { title: 'Accueil' });
+});
+
+app.listen(3000, () => {
 	console.log('Server is running: http://localhost:3000');
 });
